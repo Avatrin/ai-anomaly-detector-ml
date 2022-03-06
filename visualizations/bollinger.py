85b6b6e38e8ef948264ac7aee2b3169fcbfbd04d
Noor Hussain <avatrin@hotmail.com>
2022-03-06 21:24:28 +0100

    Update bollinger.py

--- visualizations/bollinger.py
+++ visualizations/bollinger.py
@@ -13,3 +13,7 @@ def show_bollinger_bands(df):
   plt.figure(figsize=(8,6))
   # plt.plot(df)
   plt.plot(df[["value", "sma", "bollinger_upper", "bollinger_lower"]].loc["1978-07-01 00:00:00":"1979-07-01 00:00:00"])
+
+if __name__ == "__main__":
+  df = pd.read_csv(project_root_dir/"cleaned_data2"/"drive1_torque.csv", skiprows=range(1,15), parse_dates=['key'], index_col=['key'])
+  show_bollinger_bands(df)
